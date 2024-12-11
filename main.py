68cbf3d7f9ace271c2dc7a90640022497b3c0473
Nguyễn Phi Học <85166295+phihocnguyen@users.noreply.github.com>
2024-12-11 10:46:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
-from typing import Optional
-
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from typing import List
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+import numpy as np
+import pandas
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+df = pandas.read_csv("input_time_series_data.csv")
+x = df.iloc[:, 1:25].values
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+model = tf.keras.models.load_model("model.keras")
+
+class Input(BaseModel):
+    # Assuming the model expects input as a list of floats (adjust this as needed)
+    features: List[float]
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def read_root():
+    return {"message": "API works successfully!"}
+@app.post('/predict')
+def predict(input : Input):
+    scaler = StandardScaler().fit(x)
+    features_array = np.array(input.features).reshape(1, -1)
+    new_data_scaled = scaler.transform(features_array)
+    new_data_scaled = new_data_scaled.reshape((new_data_scaled.shape[0], new_data_scaled.shape[1], 1))
+    prediction = model.predict(new_data_scaled)
+    return {
+        "prediction": prediction.tolist()
+    }
